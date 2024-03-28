f0fdc1e2af09a94ef6c66e9b710253e5727e8762
Marcio Rodrigues <mrraxa01@gmail.com>
2024-03-27 21:29:22 -0300

    backend - Implement RoomResponse and BookingResponse

--- backend/src/main/java/br/com/marciorodrigues/LakeSideHotel/response/BookedRoomResponse.java
+++
@@ -1,4 +0,0 @@
-package br.com.marciorodrigues.LakeSideHotel.response;
-
-public class BookedRoomResponse {
-}
---
+++ backend/src/main/java/br/com/marciorodrigues/LakeSideHotel/response/BookingResponse.java
@@ -0,0 +1,39 @@
+package br.com.marciorodrigues.LakeSideHotel.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookingResponse {
+    private Long id;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private String guestFullName;
+
+    private String guestEmail;
+
+    private int numOfChildrens;
+
+    private int numOfAdults;
+
+    private int totalNumOfGuest;
+
+    private String bookingConfirmationCode;
+
+    private RoomResponse room;
+
+    public BookingResponse(Long id, LocalDate checkInDate, LocalDate checkOutDate, String bookingConfirmationCode) {
+        this.id = id;
+        this.checkInDate = checkInDate;
+        this.checkOutDate = checkOutDate;
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+}
